21890574b1234ad56be86504d8549005fa5ad164
root <jaisonbaby59@gmail.com>
2022-02-16 11:45:35 +0530

    added

---
+++ terraform.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+   region     = var.instance_region
+   access_key = AKIA5MDMMSR7JRMAJ3PB
+   secret_key = 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
+   
+}
+
+module "instance1" {
+    
+    source = "https://github.com/jaison-baby/TerraForm_Learning/blob/master/variables.tf"
+    ami = var.instance_AMI  
+    instance_type = var.instance_type 
+    key_name= var.instance_key
+    vpc_security_group_ids = [aws_security_group.main.id]
+}
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+}
+module "inbondrules" {
+  source = "https://github.com/jaison-baby/TerraForm_Learning/blob/master/variables.tf"
+  count = length(var.sg_ingress_rules)
+
+  type              = "ingress"
+  from_port         = var.sg_ingress_rules[count.index].from_port
+  to_port           = var.sg_ingress_rules[count.index].to_port
+  protocol          = var.sg_ingress_rules[count.index].protocol
+  cidr_blocks       = [var.sg_ingress_rules[count.index].cidr_block]
+  description       = var.sg_ingress_rules[count.index].description
+  security_group_id = aws_security_group.main.id
+}
+
---
+++ variables.tf
@@ -0,0 +1,61 @@
+variable "instance_AMI" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "ami-0b614a5d911900a9b"
+}
+
+variable "instance_type" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "instance_key" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "test"
+}
+
+variable "instance_region" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "instance_accesskey" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "AKIA5MDMMSR7JRMAJ3PB"
+}
+variable "instance_secretkey" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "KJUOFaKn7GfwULa2o75xzejXL+EC8FuW7n4vCUsy"
+}
+variable "sg_ingress_rules" {
+    type = list(object({
+      from_port   = number
+      to_port     = number
+      protocol    = string
+      cidr_block  = string
+      description = string
+    }))
+    default     = [
+        {
+          from_port   = 22
+          to_port     = 22
+          protocol    = "tcp"
+          cidr_block  = "0.0.0.0/0"
+          description = "test"
+        },
+        {
+          from_port   = 23
+          to_port     = 23
+          protocol    = "tcp"
+          cidr_block  = "0.0.0.0/0"
+          description = "test"
+        },
+    ]
+}
+
+
