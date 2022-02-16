d854c025250fce3c8b31934cfa2544cfec2e7df8
root <jaisonbaby59@gmail.com>
2022-02-16 18:20:49 +0530

    added

---
+++ modules/terraform.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+   region     = var.instance_region
+   access_key = var.instance_accesskey
+   secret_key = var.instance_secretkey
+   
+}
+
+resource "aws_instance" "ec2_example" {
+
+    ami = var.instance_AMI  
+    instance_type = var.instance_type 
+    key_name= var.instance_key
+    vpc_security_group_ids = [aws_security_group.main.id]
+}
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+}
+resource "aws_security_group_rule" "ingress_rules" {
+  count = length(var.sg_ingress_rules)
+
+  type              = "ingress"
+  from_port         = var.sg_ingress_rules[count.index].from_port
+  to_port           = var.sg_ingress_rules[count.index].to_port
+  protocol          = var.sg_ingress_rules[count.index].protocol
+  cidr_blocks       = [var.sg_ingress_rules[count.index].cidr_block]
+  description       = var.sg_ingress_rules[count.index].description
+  security_group_id = aws_security_group.main.id
+}
+
